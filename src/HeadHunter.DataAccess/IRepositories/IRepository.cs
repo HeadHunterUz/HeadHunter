a2dbc38e0b002a7d9d17de672b01813c4cecf07a
Asrorbek <as.abrorov@gmail.com>
2024-03-28 23:35:27 +0500

    Written Repositories and downloaded Dapper and PostgreSQL packages

---
+++ src/HeadHunter.DataAccess/IRepositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿
+namespace HeadHunter.DataAccess.Repositories
+{
+    public interface IRepository<TEntity>
+    {
+        Task DeleteAsync(string tableName, int id);
+        Task<IEnumerable<TEntity>> GetAllAsync(string tableName);
+        Task<TEntity> GetByIdAsync(string tableName, int id);
+        Task InsertAsync(string tableName, TEntity entity);
+        Task UpdateAsync(string tableName, TEntity entity);
+    }
+}
