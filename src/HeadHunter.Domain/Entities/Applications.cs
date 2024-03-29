a61685c472e5d505ab41d4b0b85d842b2bb196ad
ShohsanamAbdujabborova <138601744+ShohsanamAbdujabborova@users.noreply.github.com>
2024-03-29 11:06:05 +0500

    Created Applications Entity

---
+++ src/HeadHunter.Domain/Entities/Applications.cs
@@ -0,0 +1,14 @@
+﻿using HeadHunter.Domain.Enums;
+namespace HeadHunter.Domain.Entities;
+public class Applications
+{
+    public long Id { get; set; }
+    public long UserId { get; set; }
+    public User user { get; set; }
+    public string ApplyStatus { get; set; }//enum
+    public ApplyStatus applyStatus { get; set; }
+    public long VacancyId { get; set; }
+    public JobVacancies jobVacancies { get; set; }
+    public long CompanyId { get; set; }
+    public Company company { get; set; }
+}
