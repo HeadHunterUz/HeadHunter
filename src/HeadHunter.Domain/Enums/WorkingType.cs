39a8f5672288ac04696e31447a8c7f2e9c9a9e79
ShohsanamAbdujabborova <138601744+ShohsanamAbdujabborova@users.noreply.github.com>
2024-03-29 10:57:27 +0500

    Created enum : WorkingType

--- src/HeadHunter.Domain/Enums/WorkingType.cs
+++ src/HeadHunter.Domain/Enums/WorkingType.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HeadHunter.Domain.Enums;
+﻿namespace HeadHunter.Domain.Enums;
 public enum WorkingType
 {
     Remote,
     Hybrid,
-    OnSite,
+    OnSite
 }
