6ba9b024ce452649a4bc1f122f6f388fedbba0b6
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-03-29 11:28:12 +0500

    Update Jobs.cs

--- src/HeadHunter.Domain/Entities/Jobs.cs
+++ src/HeadHunter.Domain/Entities/Jobs.cs
@@ -1,8 +1,7 @@
 ﻿using HeadHunter.Domain.Commons;
 
 namespace HeadHunter.Domain.Entities;
-public class Jobs:Auditable
+public class Jobs : Auditable
 {
-    public long Id { get; set; }
     public string Name { get; set; }
 }
