a8ca1de661519595158fb16a942e08c234c619f6
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-03-29 11:24:17 +0500

    Update BasketVacancies.cs

--- src/HeadHunter.Domain/Entities/BasketVacancies.cs
+++ src/HeadHunter.Domain/Entities/BasketVacancies.cs
@@ -2,7 +2,6 @@
 namespace HeadHunter.Domain.Entities;
 public class BasketVacancies:Auditable
 {
-    public long Id { get; set; }
     public long UserId { get; set; }
     public User user { get; set; }
 }
