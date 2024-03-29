6cc5b47efd3c45a8fe930f11001262190ebd7448
ShohsanamAbdujabborova <138601744+ShohsanamAbdujabborova@users.noreply.github.com>
2024-03-29 10:41:52 +0500

    Created IndustryCategories Entity

--- src/HeadHunter.Domain/Entities/IndustryCategories.cs
+++ src/HeadHunter.Domain/Entities/IndustryCategories.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HeadHunter.Domain.Entities;
+﻿namespace HeadHunter.Domain.Entities;
 public class IndustryCategories
 {
     public long Id { get; set; }
     public string Name { get; set; }
-    public long ParentId {  get; set; }
+    public long ParentId { get; set; }
 }
