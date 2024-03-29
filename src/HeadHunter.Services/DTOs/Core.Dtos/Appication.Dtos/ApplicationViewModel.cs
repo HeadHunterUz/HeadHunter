1a43d3d2e6c815c78809711125d91eaff86ee759
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-03-29 18:30:45 +0500

    Wrote CompaniesViewModel and fix viewmodels

--- src/HeadHunter.Services/DTOs/Core.Dtos/Appication.Dtos/ApplicationViewModel.cs
+++ src/HeadHunter.Services/DTOs/Core.Dtos/Appication.Dtos/ApplicationViewModel.cs
@@ -1,8 +1,5 @@
-﻿using HeadHunter.Domain.Entities.Jobs;
-using HeadHunter.Domain.Entities.Users;
-
-namespace HeadHunter.Services.DTOs.Core.Dtos.Appication.Dtos;
+﻿namespace HeadHunter.Services.DTOs.Core.Dtos.Appication.Dtos;
 
 public record ApplicationViewModel(
-    User User,
-    JobVacancy Vacancy);
+    UserViewModel User,
+    JobVacancyViewModel Vacancy);
---
+++ src/HeadHunter.Services/DTOs/Core.Dtos/Companies.Dtos/CompaniesViewModel.cs
@@ -0,0 +1,9 @@
+﻿using HeadHunter.Services.DTOs.Core.Dtos.Address.Dtos;
+
+namespace HeadHunter.Services.DTOs.Core.Dtos.Companies.Dtos;
+
+public record CompaniesViewModel(
+    string Name,
+    IndustryViewModel Industry,
+    string Details,
+    AddressViewModel Address);
