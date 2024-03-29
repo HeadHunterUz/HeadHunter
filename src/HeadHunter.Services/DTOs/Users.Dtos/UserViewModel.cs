2945a43d12df0ce89ed1dfda8f1fb92b5d60bcdd
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-03-29 18:45:38 +0500

    Wrote UserViewModel

---
+++ src/HeadHunter.Services/DTOs/Users.Dtos/UserViewModel.cs
@@ -0,0 +1,13 @@
+﻿using HeadHunter.Services.DTOs.Core.Dtos.Address.Dtos;
+
+namespace HeadHunter.Services.DTOs.Users.Dtos;
+
+public record UserViewModel(
+    string FirstName,
+    string LastName,
+    string Phone,
+    string Email,
+    string Password,
+    string Photo,
+    IndustryViewModel Industry,
+    AddressViewModel Address);
