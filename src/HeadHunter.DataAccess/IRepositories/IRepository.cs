356b5ef8c5894ddffd4877db1bf7f537af02ce01
Asrorbek <as.abrorov@gmail.com>
2024-03-29 05:08:28 +0500

    Added comments to the repositories

--- src/HeadHunter.DataAccess/IRepositories/IRepository.cs
+++ src/HeadHunter.DataAccess/IRepositories/IRepository.cs
@@ -1,11 +1,47 @@
-﻿
-namespace HeadHunter.DataAccess.IRepositories;
+﻿namespace HeadHunter.DataAccess.IRepositories;
 
+/// <summary>
+/// Represents a generic repository interface for CRUD operations on entities.
+/// </summary>
+/// <typeparam name="TEntity">The type of the entity.</typeparam>
 public interface IRepository<TEntity>
 {
-    Task DeleteAsync(string tableName, int id);
-    Task<IEnumerable<TEntity>> GetAllAsync(string tableName);
-    Task<TEntity> GetByIdAsync(string tableName, int id);
+    /// <summary>
+    /// Inserts an entity into the specified table asynchronously.
+    /// </summary>
+    /// <param name="tableName">The name of the table.</param>
+    /// <param name="entity">The entity to insert.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
     Task InsertAsync(string tableName, TEntity entity);
+
+    /// <summary>
+    /// Updates an entity in the specified table asynchronously.
+    /// </summary>
+    /// <param name="tableName">The name of the table.</param>
+    /// <param name="entity">The entity to update.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
     Task UpdateAsync(string tableName, TEntity entity);
+    /// <summary>
+    /// Deletes an entity asynchronously by its ID.
+    /// </summary>
+    /// <param name="tableName">The name of the table.</param>
+    /// <param name="id">The ID of the entity to delete.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    Task DeleteAsync(string tableName, int id);
+
+    /// <summary>
+    /// Retrieves all entities from the specified table asynchronously.
+    /// </summary>
+    /// <param name="tableName">The name of the table.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the collection of entities.</returns>
+    Task<IEnumerable<TEntity>> GetAllAsync(string tableName);
+
+    /// <summary>
+    /// Retrieves an entity by its ID asynchronously.
+    /// </summary>
+    /// <param name="tableName">The name of the table.</param>
+    /// <param name="id">The ID of the entity to retrieve.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the retrieved entity.</returns>
+    Task<TEntity> GetByIdAsync(string tableName, int id);
+
 }
