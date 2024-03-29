d4a43189e6fc8922bb8f62243c1dd21e6d6d4f02
ShohsanamAbdujabborova <138601744+ShohsanamAbdujabborova@users.noreply.github.com>
2024-03-29 10:16:48 +0500

    Created Industry Entities

---
+++ src/HeadHunter.Domain/Entities/Industry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadHunter.Domain.Entities;
+
+public class Industry
+{ 
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public long CategoryId {  get; set; }
+    public Category category { get; set; }
+}
