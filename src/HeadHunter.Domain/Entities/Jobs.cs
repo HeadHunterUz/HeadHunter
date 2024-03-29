4cce75d1329850635915a1194deb1378169f5d69
ShohsanamAbdujabborova <138601744+ShohsanamAbdujabborova@users.noreply.github.com>
2024-03-29 10:43:58 +0500

    Created Jobs Entity

---
+++ src/HeadHunter.Domain/Entities/Jobs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadHunter.Domain.Entities;
+
+public class Jobs
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+}
