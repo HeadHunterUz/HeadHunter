68c8913c99f648260ee41876a88b868c148805b4
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-03-29 18:32:26 +0500

    Created ExperienceCreateModel

---
+++ src/HeadHunter.Services/DTOs/Core.Dtos/Experiences.Dtos/ExperienceCreateModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadHunter.Services.DTOs.Core.Dtos.Experiences.Dtos
+{
+    internal class ExperienceCreateModel
+    {
+    }
+}
