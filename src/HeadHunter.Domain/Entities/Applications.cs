019c24696bd5ddf4e12b48a2968b19581e421da8
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-03-29 11:24:01 +0500

    Update Applications.cs

--- src/HeadHunter.Domain/Entities/Applications.cs
+++ src/HeadHunter.Domain/Entities/Applications.cs
@@ -3,10 +3,8 @@ using HeadHunter.Domain.Enums;
 namespace HeadHunter.Domain.Entities;
 public class Applications:Auditable
 {
-    public long Id { get; set; }
     public long UserId { get; set; }
     public User user { get; set; }
-    public string ApplyStatus { get; set; }//enum
     public ApplyStatus applyStatus { get; set; }
     public long VacancyId { get; set; }
     public JobVacancies jobVacancies { get; set; }
