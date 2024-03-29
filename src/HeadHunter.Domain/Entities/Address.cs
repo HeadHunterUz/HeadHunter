6dd30995cfcef0ac5125a48314040acaaef32bb5
ShohsanamAbdujabborova <138601744+ShohsanamAbdujabborova@users.noreply.github.com>
2024-03-29 10:37:55 +0500

    Created Address  Entity

---
+++ src/HeadHunter.Domain/Entities/Address.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadHunter.Domain.Entities;
+
+public class Address
+{
+    public long Id { get; set; }
+    public string Country {  get; set; }
+    public string City { get; set; }
+}
