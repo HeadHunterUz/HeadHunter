fecafcae7d520313540653b5814e85109db93a21
ShohsanamAbdujabborova <138601744+ShohsanamAbdujabborova@users.noreply.github.com>
2024-03-29 10:47:30 +0500

    Created VacancySkills Entity

--- src/HeadHunter.Domain/Entities/Jobs.cs
+++ src/HeadHunter.Domain/Entities/Jobs.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HeadHunter.Domain.Entities;
-
+﻿namespace HeadHunter.Domain.Entities;
 public class Jobs
 {
     public long Id { get; set; }
---
+++ src/HeadHunter.Domain/Entities/VacancySkills.cs
@@ -0,0 +1,8 @@
+﻿namespace HeadHunter.Domain.Entities;
+public class VacancySkills
+{
+    public long Id {  get; set; }
+    public string Name { get; set; }
+    public long VacancyId {  get; set; }
+    public JobVacanciesId jobVacanciesId {  get; set; } 
+}
