54644a816bb864321c6437af0addc02a8dee198c
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-03-29 18:10:06 +0500

    Wrote Admin DTOs

---
+++ src/HeadHunter.Services/DTOs/Admins.Dtos/AdminViewModel.cs
@@ -0,0 +1,10 @@
+﻿using HeadHunter.Domain.Entities.Core;
+
+namespace HeadHunter.Services.DTOs.Admins.Dtos;
+
+public record AdminViewModel(
+    string FirstName,
+    string LastName,
+    string Phone,
+    string Email,
+    Address Address);
