86b3402ca1a618501a3ef089d02bc168e4d14047
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-03-29 11:31:18 +0500

    Update Industry.cs

--- src/HeadHunter.Domain/Entities/Industry.cs
+++ src/HeadHunter.Domain/Entities/Industry.cs
@@ -1,13 +1,8 @@
 ﻿using HeadHunter.Domain.Commons;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HeadHunter.Domain.Entities;
 
-public class Industry:Auditable
+public class Industry : Auditable
 { 
     public long Id { get; set; }
     public string Name { get; set; }
