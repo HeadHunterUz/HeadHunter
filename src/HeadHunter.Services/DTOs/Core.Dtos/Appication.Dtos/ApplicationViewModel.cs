056452b4dad8c32ea572d5c4b4ed308318e7fa54
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-03-29 18:22:21 +0500

    Wrote ApplicationViewModel

---
+++ src/HeadHunter.Services/DTOs/Core.Dtos/Appication.Dtos/ApplicationViewModel.cs
@@ -0,0 +1,8 @@
+﻿using HeadHunter.Domain.Entities.Jobs;
+using HeadHunter.Domain.Entities.Users;
+
+namespace HeadHunter.Services.DTOs.Core.Dtos.Appication.Dtos;
+
+public record ApplicationViewModel(
+    User User,
+    JobVacancy Vacancy);
