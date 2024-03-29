1f255701220df08f841c90e09632b8f98ec46465
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-03-29 18:39:52 +0500

    Wrote Resume CreateModel

---
+++ src/HeadHunter.Services/DTOs/Core.Dtos/Resumes.Dtos/ResumeCreateModel.cs
@@ -0,0 +1,10 @@
+﻿namespace HeadHunter.Services.DTOs.Core.Dtos.Resumes.Dtos;
+
+public record ResumeCreateModel(
+    long UserId,
+    string FirstName,
+    string LastName,
+    string Description,
+    string JobTitle,
+    string Education);
+
