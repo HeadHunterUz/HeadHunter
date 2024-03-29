1d066ac5cd6aa65f3300bcc4520b695578a6501b
Asrorbek <as.abrorov@gmail.com>
2024-03-29 17:13:02 +0500

    Rewritten entities

--- src/HeadHunter.Domain/Entities/Vacancies/BasketVacancies.cs
+++ src/HeadHunter.Domain/Entities/Vacancies/BasketVacancies.cs
@@ -1,10 +1,13 @@
 ﻿using HeadHunter.Domain.Commons;
+using HeadHunter.Domain.Entities.Jobs;
 using HeadHunter.Domain.Entities.Users;
 
 namespace HeadHunter.Domain.Entities.Vacancies;
 
 public class BasketVacancies:Auditable
 {
+    public long VacancyId { get; set; }
+    public JobVacancy jobVacancy { get; set; }
     public long UserId { get; set; }
     public User user { get; set; }
 }
