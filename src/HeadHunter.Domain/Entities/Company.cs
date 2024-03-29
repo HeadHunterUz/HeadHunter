c34d7c5efea841ff9c23bcb3f92bedddf8f5409c
ShohsanamAbdujabborova <138601744+ShohsanamAbdujabborova@users.noreply.github.com>
2024-03-29 10:26:53 +0500

    Created Company Entity

---
+++ src/HeadHunter.Domain/Entities/Company.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadHunter.Domain.Entities;
+public class Company
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public long IndustryId { get; set; }
+    public Industry industry { get; set; }
+    public string Details {  get; set; }
+    public long AddressId {  get; set; }
+    public Address address { get; set; }
+   }
