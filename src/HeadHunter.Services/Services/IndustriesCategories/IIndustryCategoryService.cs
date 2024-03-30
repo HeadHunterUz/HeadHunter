65e8a8cf427a121773c8bff1d3a1e53009bf5d7d
ShohsanamAbdujabborova <138601744+ShohsanamAbdujabborova@users.noreply.github.com>
2024-03-30 21:24:41 +0500

    Created IIndustryCategories

---
+++ src/HeadHunter.Services/Services/IndustriesCategories/IIndustryCategoryService.cs
@@ -0,0 +1,12 @@
+﻿using HeadHunter.Services.DTOs.Industry.Dtos.Industries.Categories;
+
+namespace HeadHunter.Services.Services.IndustriesCategories;
+
+public interface IIndustryCategoryService
+{
+    Task<IndustryCategoryViewModel> CreateAsync(IndustryCategoryCreateModel industry);
+    Task<IndustryCategoryViewModel> UpdateAsync(long id, IndustryCategoryUpdateModel industry);
+    Task<bool> DeleteAsync(long id);
+    Task<IndustryCategoryViewModel> GetByIdAsync(long id);
+    Task<IEnumerable<IndustryCategoryViewModel>> GetAllAsync();
+}
