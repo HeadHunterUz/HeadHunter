d4257335bd6e5c032c1d2325f8a8a72fce0ff632
Asrorbek Abrorov <138248626+Asrorbek-Abrorov@users.noreply.github.com>
2024-03-30 22:58:27 +0500

    Application service fix

--- src/HeadHunter.Services/DTOs/Core.Dtos/Appication.Dtos/ApplicationViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿using HeadHunter.Services.DTOs.Jobs.Dtos.Jobs.Vacancy;
-using HeadHunter.Services.DTOs.Users.Dtos;
-
-namespace HeadHunter.Services.DTOs.Core.Dtos.Appication.Dtos;
-
-public record ApplicationViewModel(
-    UserViewModel User,
-    JobVacancyViewModel Vacancy);
