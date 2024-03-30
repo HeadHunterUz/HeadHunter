94df53730fd0c2a31e5478246cb8d67508a0f834
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-03-30 18:19:26 +0500

    Wrote CustomExceptions

--- src/HeadHunter.Services/Exceptions/CustomExceptions.cs
+++ src/HeadHunter.Services/Exceptions/CustomExceptions.cs
@@ -1,6 +1,11 @@
-﻿namespace HeadHunter.Services.Exceptions
+﻿namespace HeadHunter.Services.Exceptions;
+
+public class CustomException : Exception
 {
-    internal class CustomExceptions
+    public int StatusCode { get; set; }
+
+    public CustomException(int statusCode, string message) : base(message)
     {
+        this.StatusCode = statusCode;
     }
 }
