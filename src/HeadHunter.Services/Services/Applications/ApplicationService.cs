52a73eca68f7441c87507781651eb8acdeebe2a1
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-03-30 19:03:13 +0500

    CreatedApplicationService

---
+++ src/HeadHunter.Services/Services/Applications/ApplicationService.cs
@@ -0,0 +1,31 @@
+﻿using HeadHunter.Services.DTOs.Core.Dtos.Appication.Dtos;
+
+namespace HeadHunter.Services.Services.Applications;
+
+public class ApplicationService : IApplicationService
+{
+    public Task<ApplicationViewModel> CreateAsync(ApplicationUpdateModel address)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<ApplicationViewModel>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ApplicationViewModel> GetByIdAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ApplicationViewModel> UpdateAsync(long id, ApplicationUpdateModel address)
+    {
+        throw new NotImplementedException();
+    }
+}
