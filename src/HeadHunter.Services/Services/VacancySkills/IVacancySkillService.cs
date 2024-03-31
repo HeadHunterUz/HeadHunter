129a2ca7e74a4991193ad85e655870b67a828861
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-03-31 23:23:38 +0500

    Wrote VacancySkillService

--- src/HeadHunter.Services/Services/VacancySkills/IVacancySkillService.cs
+++ src/HeadHunter.Services/Services/VacancySkills/IVacancySkillService.cs
@@ -4,9 +4,9 @@ namespace HeadHunter.Services.Services.VacancySkills;
 
 public interface IVacancySkillService
 {
-    Task<VacancySkillViewModel> CreateVacancySkillAsync(VacancySkillCreateModel vacancySkill);
-    Task<VacancySkillViewModel> UpdateSkillAsync(long id, VacancySkillUpdateModel vacancySkill);
-    Task<bool> CreateVacancySkillAsync(long id);
+    Task<VacancySkillViewModel> CreateAsync(VacancySkillCreateModel vacancySkill);
+    Task<VacancySkillViewModel> UpdateAsync(long id, VacancySkillUpdateModel vacancySkill);
+    Task<bool> DeleteAsync(long id);
     Task<VacancySkillViewModel> GetByIdAsync(long id);
     Task<IEnumerable<VacancySkillViewModel>> GetAllAsync();
 }
--- src/HeadHunter.Services/Services/VacancySkills/VacancySkillService.cs
+++ src/HeadHunter.Services/Services/VacancySkills/VacancySkillService.cs
@@ -1,31 +1,80 @@
-﻿using HeadHunter.Services.DTOs.Vacancies.Dtos.VacancySkills;
+﻿using AutoMapper;
+using HeadHunter.DataAccess.IRepositories;
+using HeadHunter.Domain.Entities.Vacancies;
+using HeadHunter.Services.DTOs.Vacancies.Dtos.VacancySkills;
+using HeadHunter.Services.Exceptions;
+using HeadHunter.Services.Services.JobVacancies;
 
 namespace HeadHunter.Services.Services.VacancySkills;
 
 public class VacancySkillService : IVacancySkillService
 {
-    public Task<VacancySkillViewModel> CreateVacancySkillAsync(VacancySkillCreateModel vacancySkill)
+
+    private IMapper mapper;
+    private IRepository<VacancySkill> repository;
+    private IJobVacancyService jobVacancyService;
+    private readonly string vacancyskilltable = DataAccess.Constants.VacancySkillTableName;
+
+
+    public async Task<VacancySkillViewModel> CreateAsync(VacancySkillCreateModel vacancySkill)
     {
-        throw new NotImplementedException();
+        var existJobVacancy = await jobVacancyService.GetByIdAsync(vacancySkill.JobVacancyId);
+
+        var existVacancySkill = (await repository.GetAllAsync(vacancyskilltable))
+            .FirstOrDefault(v => v.VacancyId == vacancySkill.JobVacancyId && v.Name == vacancySkill.Name);
+
+        if (existVacancySkill != null)
+            throw new CustomException(409, "VacancySkill is already exists");
+
+        var created = repository.InsertAsync(vacancyskilltable, mapper.Map<VacancySkill>(vacancySkill));
+
+        return new VacancySkillViewModel
+        {
+            Id = created.Id,
+            Name = vacancySkill.Name,
+            JobVacancy = existJobVacancy
+        };
     }
 
-    public Task<bool> CreateVacancySkillAsync(long id)
+    public async Task<bool> DeleteAsync(long id)
     {
-        throw new NotImplementedException();
+        var existVacancySkill = await repository.GetByIdAsync(vacancyskilltable, id)
+            ?? throw new CustomException(404, "Not found");
+
+        await repository.DeleteAsync(vacancyskilltable, id);
+        return true;
     }
 
-    public Task<IEnumerable<VacancySkillViewModel>> GetAllAsync()
+    public async Task<IEnumerable<VacancySkillViewModel>> GetAllAsync()
     {
-        throw new NotImplementedException();
+        var VacancySkills = await repository.GetAllAsync(vacancyskilltable);
+
+        return mapper.Map<IEnumerable<VacancySkillViewModel>>(VacancySkills);
     }
 
-    public Task<VacancySkillViewModel> GetByIdAsync(long id)
+    public async Task<VacancySkillViewModel> GetByIdAsync(long id)
     {
-        throw new NotImplementedException();
+        var existVacancySkill = await repository.GetByIdAsync(vacancyskilltable, id)
+            ?? throw new CustomException(404, "Not found");
+        return mapper.Map<VacancySkillViewModel>(existVacancySkill);
+
     }
 
-    public Task<VacancySkillViewModel> UpdateSkillAsync(long id, VacancySkillUpdateModel vacancySkill)
+    public async Task<VacancySkillViewModel> UpdateAsync(long id, VacancySkillUpdateModel vacancySkill)
     {
-        throw new NotImplementedException();
+        var existJobVacancy = await jobVacancyService.GetByIdAsync(vacancySkill.JobVacancyId);
+
+        var existVacancySkill = await repository.GetByIdAsync(vacancyskilltable, id)
+           ?? throw new CustomException(404, "Not found");
+
+        var mapped = mapper.Map(vacancySkill, existVacancySkill);
+        var updated = await repository.UpdateAsync(vacancyskilltable, mapped);
+
+        return new VacancySkillViewModel
+        {
+            Id = id,
+            JobVacancy = existJobVacancy,
+            Name = updated.Name,
+        };
     }
 }
