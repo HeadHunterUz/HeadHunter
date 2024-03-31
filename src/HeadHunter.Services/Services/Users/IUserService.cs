e8d7a16667cab963e8d96895dc66825f0a77c968
ShohsanamAbdujabborova <138601744+ShohsanamAbdujabborova@users.noreply.github.com>
2024-03-31 23:03:28 +0500

    Created :IUserService

---
+++ src/HeadHunter.Services/Services/Users/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using HeadHunter.Services.DTOs.Core.Dtos.Resumes.Dtos;
+using HeadHunter.Services.DTOs.Users.Dtos;
+
+namespace HeadHunter.Services.Services.Users;
+public interface IUserService
+{
+    Task<UserViewModel> CreateAsync(UserCreateModel user);
+    Task<UserViewModel> UpdateAsync(long id,UserUpdateModel user);
+    Task<bool> DeleteAsync(long id);
+    Task<UserViewModel> GetByIdAsync(long id);
+    Task<IEnumerable<UserViewModel>> GetAllAsync();
+}
