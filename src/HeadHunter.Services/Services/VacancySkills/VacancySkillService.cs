7a67545041c0c7e74a47751606f0bb93d78ba281
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-03-31 22:36:50 +0500

    Created VacancySkillService

---
+++ src/HeadHunter.Services/Services/VacancySkills/VacancySkillService.cs
@@ -0,0 +1,31 @@
+﻿using HeadHunter.Services.DTOs.Vacancies.Dtos.VacancySkills;
+
+namespace HeadHunter.Services.Services.VacancySkills;
+
+public class VacancySkillService : IVacancySkillService
+{
+    public Task<VacancySkillViewModel> CreateVacancySkillAsync(VacancySkillCreateModel vacancySkill)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> CreateVacancySkillAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<VacancySkillViewModel>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<VacancySkillViewModel> GetByIdAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<VacancySkillViewModel> UpdateSkillAsync(long id, VacancySkillUpdateModel vacancySkill)
+    {
+        throw new NotImplementedException();
+    }
+}
