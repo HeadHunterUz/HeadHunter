ee5548f612ba1165b90ca6c6ab853ca5ff80a0f2
ShohsanamAbdujabborova <138601744+ShohsanamAbdujabborova@users.noreply.github.com>
2024-04-01 00:02:17 +0500

    Wrote comments for Admin Entity

--- src/HeadHunter.Domain/Entities/Admins/Admin.cs
+++ src/HeadHunter.Domain/Entities/Admins/Admin.cs
@@ -5,10 +5,28 @@ namespace HeadHunter.Domain.Entities.Admins;
 
 public class Admin : Auditable
 {
+    /// <summary>
+    /// admin's FirstName
+    /// </summary>
     public string FirstName { get; set; }
+    /// <summary>
+    /// admin's LastName
+    /// </summary>
     public string LastName { get; set; }
+    /// <summary>
+    /// admin's PhoneNumber
+    /// </summary>
     public string Phone { get; set; }
+    /// <summary>
+    /// admin's Email
+    /// </summary>
     public string Email { get; set; }
+    /// <summary>
+    /// admin's Password
+    /// </summary>
     public string Password { get; set; }
+    /// <summary>
+    /// admin's Address
+    /// </summary>
     public long AddressId { get; set; }
 }
