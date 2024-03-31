af54afc28d93d0277899fa055555abfd2ca07072
ShohsanamAbdujabborova <138601744+ShohsanamAbdujabborova@users.noreply.github.com>
2024-03-31 21:44:35 +0500

    Created IExperienceService

---
+++ src/HeadHunter.Services/Services/Experiences/IExperienceService.cs
@@ -0,0 +1,11 @@
+﻿using HeadHunter.Services.DTOs.Core.Dtos.Experiences.Dtos;
+
+namespace HeadHunter.Services.Services.Experiences;
+public interface IExperienceService
+{
+    Task<ExperienceViewModel> CreateAsync(ExperienceCreateModel experience);
+    Task<ExperienceViewModel> UpdateAsync(long id, ExperienceUpdateModel experience);
+    Task<bool> DeleteAsync(long id);
+    Task<ExperienceViewModel> GetByIdAsync(long id);
+    Task<IEnumerable<ExperienceViewModel>> GetAllAsync();
+}
