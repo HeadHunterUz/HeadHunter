dfc6471d5c3527e6ada7443e5411697697058f31
ShohsanamAbdujabborova <138601744+ShohsanamAbdujabborova@users.noreply.github.com>
2024-03-31 12:15:31 +0500

    Created : IndustryCategoryService

--- src/HeadHunter.Domain/Entities/Industries/IndustryCategory.cs
+++ src/HeadHunter.Domain/Entities/Industries/IndustryCategory.cs
@@ -2,7 +2,7 @@
 
 namespace HeadHunter.Domain.Entities.Industries;
 
-public class IndustryCategories : Auditable
+public class IndustryCategory : Auditable
 {
     public string Name { get; set; }
     public long ParentId { get; set; }
