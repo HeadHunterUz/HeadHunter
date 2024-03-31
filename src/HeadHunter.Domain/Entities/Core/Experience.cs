87e8b468abd2842f5eeb229cf6145ad2764a2c77
ShohsanamAbdujabborova <138601744+ShohsanamAbdujabborova@users.noreply.github.com>
2024-04-01 00:10:55 +0500

    Comments for Experience Entity

--- src/HeadHunter.Domain/Entities/Core/Experience.cs
+++ src/HeadHunter.Domain/Entities/Core/Experience.cs
@@ -5,10 +5,28 @@ namespace HeadHunter.Domain.Entities.Core;
 
 public class Experience : Auditable
 {
+    /// <summary>
+    /// User's Id for experience 
+    /// </summary>
     public long UserId { get; set; }
+    /// <summary>
+    /// Company's Id
+    /// </summary>
     public long CompanyId { get; set; }
+    /// <summary>
+    /// JobTitle
+    /// </summary>
     public string JobTitle { get; set; }
+    /// <summary>
+    /// Job position for experience
+    /// </summary>
     public string Position { get; set; }
+    /// <summary>
+    /// Start Time
+    /// </summary>
     public DateTime StartTime { get; set; }
+    /// <summary>
+    /// End Time 
+    /// </summary>
     public DateTime? EndTime { get; set; }
 }
