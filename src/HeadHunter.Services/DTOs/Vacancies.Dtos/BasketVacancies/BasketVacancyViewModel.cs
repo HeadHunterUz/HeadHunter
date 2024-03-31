ac80a25a8ffe42a865cd403991f6426fc0f6a6e8
ShohsanamAbdujabborova <138601744+ShohsanamAbdujabborova@users.noreply.github.com>
2024-03-31 10:26:36 +0500

    Changed : BasketVacancies DTO's

--- src/HeadHunter.Services/DTOs/Vacancies.Dtos/BasketVacancies/BasketVacancyViewModel.cs
+++ src/HeadHunter.Services/DTOs/Vacancies.Dtos/BasketVacancies/BasketVacancyViewModel.cs
@@ -3,9 +3,9 @@ using HeadHunter.Services.DTOs.Users.Dtos;
 
 namespace HeadHunter.Services.DTOs.Vacancies.Dtos.BasketVacancies;
 
-public record BasketVacancyViewModel(
-    long Id,
-    long vacancyId,
-    JobVacancyViewModel JobVacancy,
-    long UserId,
-    UserViewModel User);
+public record BasketVacancyViewModel
+{
+    public long Id { get; set; }
+    public JobVacancyViewModel JobVacancy { get; set; }
+    public UserViewModel User { get; set; }
+}
