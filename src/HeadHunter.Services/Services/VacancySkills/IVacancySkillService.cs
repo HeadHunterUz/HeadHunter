64a510d99a93ff8929c84df5ddd996e6e8ba04d4
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-03-31 22:36:11 +0500

    Wrote Interface

--- src/HeadHunter.Services/Services/VacancySkills/IVacancySkillService.cs
+++ src/HeadHunter.Services/Services/VacancySkills/IVacancySkillService.cs
@@ -1,6 +1,12 @@
-﻿namespace HeadHunter.Services.Services.VacancySkills;
+﻿using HeadHunter.Services.DTOs.Vacancies.Dtos.VacancySkills;
+
+namespace HeadHunter.Services.Services.VacancySkills;
 
 public interface IVacancySkillService
 {
-
+    Task<VacancySkillViewModel> CreateVacancySkillAsync(VacancySkillCreateModel vacancySkill);
+    Task<VacancySkillViewModel> UpdateSkillAsync(long id, VacancySkillUpdateModel vacancySkill);
+    Task<bool> CreateVacancySkillAsync(long id);
+    Task<VacancySkillViewModel> GetByIdAsync(long id);
+    Task<IEnumerable<VacancySkillViewModel>> GetAllAsync();
 }
